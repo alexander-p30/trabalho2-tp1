13992d29afc54ccf80c8ac75cd328571864c14c6
alexander-p30 <alexanderp30e@gmail.com>
2020-09-22 11:03:17 -0300

    :sparkles: Implementation of CodigoDeAgencia

--- Domains.cpp
+++ Domains.cpp
@@ -1,37 +1,32 @@
 #include "Domains.h"
 #include <iostream>
+using namespace std;
 
 /** 
  * Método construtor da classe Cep.
- * @param cepNumber Número de cpf a ser guardado na classe.
- * @throw invalid_argument Se o método de validação do número falhar, lança exceção.
- * 
-*/
-Cep::Cep(int cepNumber) {
-  try{
-    if(validate(cepNumber)){ 
-      number = cepNumber;
-    } else {
-      throw std::invalid_argument("valor informado para CEP não condiz com nenhuma das faixas.");
-    }
-  } catch(const std::invalid_argument& err) {
-    std::cerr << "Argumento inválido: " << err.what();
-  } 
-}
-
-
-/**
- * Valida o número de entrada para a criação de CPF.
- * @param cepNumber É um número inteiro, que deve estar contido em um dos seguintes invervalos: [1000000,5999999], 
+ * @param cepNumber Número de cpf a ser guardado na classe. É um número inteiro, que deve estar contido
+ em um dos seguintes invervalos: [1000000,5999999], 
  [8000000,8499999], 
  [20000000,26600999], 
  [70000000,70999999], 
  [40000000,41999999],
  [60000000,60999999]
- * @return true Se o parâmetro está contido em algum dos intervalos possíveis.
- * @return false Se o valor parâmetro não se encaixou em nenhum dos intervalos possíveis. 
+ * @throw invalid_argument Se o método de validação do número falhar, lança exceção.
+ * 
 */
-bool Cep::validate(int cepNumber) {
+Cep::Cep(int cepNumber) {
+  try{
+    if(validar(cepNumber)){ 
+      number = cepNumber;
+    } else {
+      throw invalid_argument("valor informado para CEP não condiz com nenhuma das faixas.");
+    }
+  } catch(const invalid_argument& err) {
+    cerr << "Argumento inválido: " << err.what();
+  } 
+}
+
+bool Cep::validar(int cepNumber) {
   const int numDeFaixas = 6;
   const int faixas[numDeFaixas][2] = {
     1000000,5999999,
@@ -58,4 +53,37 @@ bool Cep::validate(int cepNumber) {
 */
 int Cep::getNumber() {
   return number;
+}
+
+/** 
+ * Método construtor da classe CodigoDeAgencia.
+ * @param code Representa o código de agência. Deve estar no formato XXXX, numérico, diferente de 0000.
+ * @throw invalid_argument Lança exceção se formato do parâmetro não estiver correto.
+*/
+CodigoDeAgencia::CodigoDeAgencia(string code) {
+  try{
+    if(validar(code)){ 
+      this->code = code;
+    } else {
+      throw invalid_argument("valor informado para Código de Agência não é válido.");
+    }
+  } catch(const invalid_argument& err) {
+    cerr << "Argumento inválido: " << err.what();
+  } 
+}
+
+bool CodigoDeAgencia::validar(string code) {
+  if(code.length() != 4 || code == "0000") {
+    return false;
+  }
+
+  return true;
+}
+
+/** 
+ * Método que retorna string do código de agência.
+ * @return String do código de agência
+*/
+string CodigoDeAgencia::getCode() {
+  return code;
 }
--- Domains.h
+++ Domains.h
@@ -1,17 +1,24 @@
+#include <string>
+using namespace std;
 /** 
  * Classe de números de CPF
 */
 class Cep {
 private:
   int number;
-  bool validate(int cepNumber);
+  bool validar(int cepNumber);
 public:
   Cep(int cepNumber);
   int getNumber();
 };
 
 class CodigoDeAgencia {
-
+private:
+  string code;
+  bool validar(string code);
+public:
+  CodigoDeAgencia(string code);
+  string getCode();
 };
 
 class CodigoDeBanco {
