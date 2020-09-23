1a3dd41d177ad91bf364a5b2826b827fa369ad46
alexander-p30 <alexanderp30e@gmail.com>
2020-09-23 16:01:59 -0300

    :white_check_mark: Tests for Cep

--- tests/Domains.cpp
+++ tests/Domains.cpp
@@ -1,2 +1,43 @@
+#include <iostream>
 #include "Domains.h"
 using namespace std;
+
+/** 
+ * Método construtor para a classe de teste de unidade para CEP.
+*/
+TUCep::TUCep() {
+  estado = SUCESSO;
+  cep = new Cep();
+}
+
+void TUCep::cenarioValido() {
+  try {
+    cep->setNumero(CEP_VALIDO);
+  } catch(const invalid_argument& err) {
+    estado = FALHA;
+  }
+}
+
+void TUCep::cenarioInvalido() {
+  try {
+    cep->setNumero(CEP_INVALIDO);
+    estado = FALHA;
+  } catch(const invalid_argument& err) {
+    return;
+  }
+}
+
+/** 
+ * Método de execução de teste de unidade para domínio CEP. São executados dois testes:
+ * - Um para valor válido para CEP;
+ * - Um para valor inválido para CEP
+ * @return 1 Caso todos os testes tenham obtido sucesso.
+ * @return 2 Caso pelo menos um dos testes tenha obtido falha.
+*/
+int TUCep::executar() {
+  cenarioValido();
+  cenarioInvalido();
+  delete cep;
+
+  return estado;
+}
---
+++ tests/Domains.h
@@ -0,0 +1,87 @@
+#include "../Domains.h"
+#include <string>
+using namespace std;
+
+/** 
+ * Classe de teste para domínio de números de Cep.
+*/
+class TUCep {
+private:
+  static const int CEP_VALIDO = 1562348;
+  static const int CEP_INVALIDO = 10000;
+  
+  int estado;
+  Cep *cep;
+  
+  void cenarioValido();
+  void cenarioInvalido();
+public:
+  TUCep();
+  int executar();
+  static const int SUCESSO = 1;
+  static const int FALHA = 0;
+};
+
+/** 
+ * Classe de teste para domínio de códigos de agência.
+*/
+class TUCodigoDeAgencia {
+private:
+public:
+};
+
+/** 
+ * Classe de teste para domínio de códigos de banco.
+*/
+class TUCodigoDeBanco {
+private:
+public:
+};
+
+/** 
+ * Classe de teste para domínio de números de CPF.
+*/
+class TUCpf {
+private:
+public:
+};
+
+/** 
+ * Classe de teste para domínio de códigos de Emissor.
+*/
+class TUEmissor {
+private:
+public:
+};
+
+/** 
+ * Classe de teste para domínio de Horários.
+*/
+class TUHorario {
+private:
+public:
+};
+
+/** 
+ * Classe de teste para domínio de números de conta.
+*/
+class TUNumeroDeConta {
+private:
+public:
+};
+
+/** 
+ * Classe de teste para domínio de senhas.
+*/
+class TUSenha {
+private:
+public:
+};
+
+/** 
+ * Classe de teste para domínio de valores de aplicação.
+*/
+class TUValorDeAplicacao {
+private:
+public:
+};
