f6b84d6fa30d32e1a435a4b32323d59bf2e1df91
alexander-p30 <alexanderp30e@gmail.com>
2020-10-17 16:47:54 -0300

    :sparkles: Runs tests on main file

--- source/main.cpp
+++ source/main.cpp
@@ -1,17 +1,29 @@
 #include "../tests/domain_tests.h"
+#include "../tests/entity_tests.h"
 #include <iostream>
 
 int main() {
   TestesDeDominio testesDominio;
+  TestesDeEntidade testesEntidade;
   cout.precision(15);
 
   switch (testesDominio.executar())
   {
   case TestesDeDominio::SUCESSO:
-    cout << endl << "Todos os testes de domínio foram bem sucedidos!" << endl;
+    cout << endl << "Todos os testes de domínio foram bem sucedidos!" << endl << endl;
     break;
   case TestesDeDominio::FALHA:
-    cout << endl << "Algum dos testes de domínio apresentou falha!" << endl;
+    cout << endl << "Algum dos testes de domínio apresentou falha!" << endl << endl;
+    break;
+  }
+
+  switch (testesEntidade.executar())
+  {
+  case TestesDeEntidade::SUCESSO:
+    cout << endl << "Todos os testes de entidade foram bem sucedidos!" << endl << endl;
+    break;
+  case TestesDeEntidade::FALHA:
+    cout << endl << "Algum dos testes de entidade apresentou falha!" << endl << endl;
     break;
   }
 
